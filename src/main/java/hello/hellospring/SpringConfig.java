50e811ee33af82dc2d02f8aa123a38227d270652
Min Hankyul <denirokr22@gmail.com>
2023-01-16 14:59:24 +0900

    스프링 입문
    공부내용:
    Spring Data JPA 통해 인터페이스만으로 MemberRepository 구현

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -11,28 +11,32 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-
 //    private DataSource dataSource;
-    private EntityManager em;
+//    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
 //    public SpringConfig(DataSource dataSource) {
 //        this.dataSource = dataSource;
 //    }
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//
+//    }
 }
